5811a1e96b303873f58ae0c8b6c0fc0867a9b25a
Bar Maizel <Barmaizel@rashi.org.il>
2023-07-31 13:03:35 +0300

    up

---
+++ src/app/Components/Skills.js
@@ -0,0 +1,22 @@
+export default function Projects() {
+    return (
+      <main id="skills" className="min-h-screen center">
+        <h1>Programming Languages</h1>
+        <p>Python</p>
+        <p>React</p>
+        <p>React Native</p>
+        <p>Node.js</p>
+        <p>Javscript</p>
+        <p>CSS</p>
+        <p>HTML</p>
+        <h1>Tools</h1>
+        <p>Firestore</p>
+        <p>AWS</p>
+        <p>Docker</p>
+        <p>MongoDB</p>
+        <p>Postman</p>
+        <p>Twilio API</p>
+      </main>
+    )
+ }
+  
