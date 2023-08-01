5d508377d335973d9ea8caf84ad2c5c3627f437c
Bar856 <barmaizel1@gmail.com>
2023-08-01 20:28:09 +0300

    about fix

--- src/app/Components/About.js
+++ src/app/Components/About.js
@@ -3,7 +3,7 @@ export default function About() {
       <main id="home"  className="flex flex-col center">
         <h2>Hi!</h2><br/>
         <p className="w-3/4">
-          I'm Bar, a Python & Full Stack developer with experience in both front-end and back-end development.<br/> 
+          I'm Bar, a Python `&apos;` Full Stack developer with experience in both front-end and back-end development.<br/> 
           I am skilled in React, Node.js, and noSQL, and I have a passion for building complex systems and constantly learning and improving my skills.<br/>
           Currently, I am working at MinervaXR, where I use my expertise to develop and maintain web applications for clients in the healthcare industry.<br/>
           I am focused on the backend, where I build robust and scalable server-side systems.<br/>
