52601bc387705c9f921fc80a76fa3b9cdfb4ba38
Bar856 <barmaizel1@gmail.com>
2023-10-11 17:13:41 +0300

    fix

--- src/app/Components/About.js
+++ src/app/Components/About.js
@@ -6,7 +6,7 @@ export default function About() {
           I&apos;m Bar, a Python & Full Stack developer with experience in both front-end and back-end development.<br/> 
           I am skilled in React, Node.js, and noSQL, and I have a passion for building complex systems and constantly learning and improving my skills.<br/>
           Currently, I am working at MinervaXR, where I use my expertise to develop and maintain web applications for clients in the healthcare industry.<br/>
-          I am focused on the backend, where I build robust and scalable server-side systems.<br/>
+         <br/>
         </p>
       </main>
     )
